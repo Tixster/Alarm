4f4d5324e4ee73c3670b5c1fa8d2b1bb5bacce75
Тила Кирилл <tilakirill98@gmail.com>
2022-02-09 19:51:33 +0200

    Initial Commit

---
+++ SleepTime/SleepTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepTimeApp.swift
+//  SleepTime
+//
+//  Created by Кирилл Тила on 09.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SleepTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
